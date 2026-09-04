3accd92a5c8e702f3ac4c93e2309fc49c051aa8d
Gust-AS <gustavoalvess@unipam.edu.br>
2026-09-04 23:25:30 +0000

    aula 0409.26

--- Aula 02.09/Fila.java
+++ Aula 02.09/Fila.java
@@ -12,4 +12,75 @@ public class Fila<T>{
         this.primeiroNo = null;
         this.ultimoNo = null;
     }
+
+
+    public void enfilerar(T dado){
+        No<T> novoNo = new No<T>(dado);
+
+        if (primeiroNo == null) {
+            primeiroNo = novoNo;
+            ultimoNo = novoNo;
+        }else{
+            ultimoNo.setNextNo(novoNo);
+            ultimoNo = novoNo;
+        }
+    }
+
+    public void imprimeFila(){
+        if (primeiroNo == null) {
+            System.out.println("Fila Vazia!!");
+        }else{
+            No<T> aux = primeiroNo;
+            while (aux != null) {
+                System.out.println(aux.toString());
+                aux = aux.getNextNo();
+            }
+        }
+    }
+
+    public T desenfilerar(){
+        if (primeiroNo == null) {
+            System.out.println("Fila Vazia");
+            return  null;
+        }
+        T dado = primeiroNo.getDado();
+        primeiroNo = primeiroNo.getNextNo();
+
+        if (primeiroNo == null) {
+            ultimoNo = primeiroNo;
+        }
+
+        return dado;
+    }
+
+    
+    //gets e sets
+    public String getNomeFila() {
+        return nomeFila;
+    }
+
+    public void setNomeFila(String nomeFila) {
+        this.nomeFila = nomeFila;
+    }
+
+    public No<T> getPrimeiroNo() {
+        return primeiroNo;
+    }
+
+    public void setPrimeiroNo(No<T> primeiroNo) {
+        this.primeiroNo = primeiroNo;
+    }
+
+    public No<T> getUltimoNo() {
+        return ultimoNo;
+    }
+
+    public void setUltimoNo(No<T> ultimoNo) {
+        this.ultimoNo = ultimoNo;
+    }
+    //gets e sets
+
+    
+
+
 }
--- Aula 02.09/Principal.java
+++ Aula 02.09/Principal.java
@@ -2,5 +2,21 @@ public class Principal{
     public static void main(String[] args){
         Fila<Integer> fila = new Fila<Integer>("Números Inteiros");
 
+        fila.enfilerar(1);
+        fila.enfilerar(2);
+        fila.enfilerar(3);
+         fila.enfilerar(4);
+
+        fila.imprimeFila();
+
+        /*
+        System.out.println("Dado:" + fila.desenfilerar() + " Removido");
+        */
+
+       Integer total = 1;
+       for(int i = 0; i < 4; i++){
+        total *= fila.desenfilerar();
+       }
+       System.out.println("Total:" + total);
     }
 }
